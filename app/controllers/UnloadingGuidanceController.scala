0e47fe6e387f92e7c49dbe764bda96bbcafd89c2
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2022-04-08 12:49:17 +0100

    CTCP-132: Removed Unloading guidance njk file

--- app/controllers/UnloadingGuidanceController.scala
+++ app/controllers/UnloadingGuidanceController.scala
@@ -17,18 +17,15 @@
 package controllers
 
 import controllers.actions._
-
-import javax.inject.Inject
 import models.{ArrivalId, Mode}
 import navigation.Navigator
 import pages.UnloadingGuidancePage
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import renderer.Renderer
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.UnloadingGuidanceView
 
+import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 class UnloadingGuidanceController @Inject() (
