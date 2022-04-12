cfbfce766363aadc05b67b9121a9231093b4814f
carrielies <carrielies@users.noreply.github.com>
2022-04-12 17:31:00 +0100

    CTCP-132 Migrate Unauthorised to Twirl

--- app/controllers/UnauthorisedController.scala
+++ app/controllers/UnauthorisedController.scala
@@ -19,20 +19,20 @@ package controllers
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import renderer.Renderer
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.UnauthorisedView
 
 import scala.concurrent.ExecutionContext
 
 class UnauthorisedController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  renderer: Renderer
+  view: UnauthorisedView
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad(): Action[AnyContent] = Action.async {
+  def onPageLoad(): Action[AnyContent] = Action {
     implicit request =>
-      renderer.render("unauthorised.njk").map(Ok(_))
+      Ok(view())
   }
 }
