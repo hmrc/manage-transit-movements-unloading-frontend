4fc02622672768f5c5613db847eabcb9e9934900
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-05-09 12:33:46 +0100

    Minor bug fix with link with trailing space. Plus some general view tidy up.

--- it/connectors/WireMockSuite.scala
+++ it/connectors/WireMockSuite.scala
@@ -19,20 +19,11 @@ package connectors
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
-import play.api.inject.guice.{GuiceApplicationBuilder, GuiceableModule}
 
 trait WireMockSuite extends BeforeAndAfterAll with BeforeAndAfterEach {
   this: Suite =>
 
   protected val server: WireMockServer = new WireMockServer(wireMockConfig().dynamicPort())
-
-  protected def portConfigKey: String
-
-  protected lazy val appBuilder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
-    .configure(portConfigKey -> server.port().toString)
-    .overrides(bindings: _*)
-
-  protected def bindings: Seq[GuiceableModule] = Seq.empty
 
   override def beforeAll(): Unit = {
     server.start()
