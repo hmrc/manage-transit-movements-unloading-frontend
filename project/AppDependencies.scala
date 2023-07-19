3ab257634cefac352adfd888a5948bb72bba328c
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2023-07-19 16:36:35 +0100

    revert flexmark-all update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -30,7 +30,7 @@ object AppDependencies {
     "org.pegdown"                 % "pegdown"                  % "1.6.0",
     "org.jsoup"                   % "jsoup"                    % "1.15.4",
     "com.github.tomakehurst"      % "wiremock-standalone"      % "2.27.2",
-    "com.vladsch.flexmark"        % "flexmark-all"             % "0.64.6"
+    "com.vladsch.flexmark"        % "flexmark-all"             % "0.64.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
