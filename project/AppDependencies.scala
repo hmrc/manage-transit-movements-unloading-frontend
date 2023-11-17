27e8cbadac58ba1815dc991c8cf449a074b6f294
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2023-11-17 08:24:53 +0000

    CTCP-4418: Updating CSP directive.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val mongoVersion = "1.3.0"
-  private val bootstrapVersion = "7.22.0"
+  private val mongoVersion = "1.4.0"
+  private val bootstrapVersion = "7.23.0"
   private val catsVersion = "2.9.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -14,19 +14,19 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "com.lucidchart"       %% "xtract"                         % "2.2.1",
     "uk.gov.hmrc"          %% "play-allowlist-filter"          % "1.2.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc"             % "7.23.0-play-28",
-    "uk.gov.hmrc"          %% "crypto-json-play-28"            % "7.3.0"
+    "uk.gov.hmrc"          %% "play-frontend-hmrc"             % "7.27.0-play-28",
+    "uk.gov.hmrc"          %% "crypto-json-play-28"            % "7.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatest"              %% "scalatest"                % "3.2.15",
+    "org.scalatest"              %% "scalatest"                % "3.2.17",
     "uk.gov.hmrc"                %% "bootstrap-test-play-28"   % bootstrapVersion,
     "com.typesafe.play"          %% "play-test"                % PlayVersion.current,
     "org.mockito"                 % "mockito-core"             % "5.2.0",
-    "org.scalatestplus"          %% "mockito-4-6"              % "3.2.15.0",
+    "org.scalatestplus"          %% "mockito-4-11"             % "3.2.17.0",
     "org.scalacheck"             %% "scalacheck"               % "1.17.0",
-    "org.scalatestplus"          %% "scalacheck-1-17"          % "3.2.15.0",
+    "org.scalatestplus"          %% "scalacheck-1-17"          % "3.2.17.0",
     "io.github.wolfendale"       %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.pegdown"                 % "pegdown"                  % "1.6.0",
     "org.jsoup"                   % "jsoup"                    % "1.15.4",
