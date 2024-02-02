bb3074c2c8139e4dd9c5f17aaf1b2114ddec5125
joecefnhedog <joecefnhedog@gmail.com>
2024-02-02 12:11:16 +0000

    CTCP-4545: Initial commit

---
+++ app/controllers/houseConsignment/index/items/PackageTypeController.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.houseConsignment.index.items
+
+import controllers.actions._
+import forms.SelectableFormProvider
+import models.reference.PackageType
+import models.requests.MandatoryDataRequest
+import models.{ArrivalId, Index, Mode}
+import navigation.Navigator
+import pages.houseConsignment.index.items.PackageTypePage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import repositories.SessionRepository
+import services.PackagesService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.houseConsignment.index.items.PackageTypeView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class PackageTypeController @Inject() (
+  override val messagesApi: MessagesApi,
+  implicit val sessionRepository: SessionRepository,
+  actions: Actions,
+  navigator: Navigator,
+  val controllerComponents: MessagesControllerComponents,
+  formProvider: SelectableFormProvider,
+  service: PackagesService,
+  view: PackageTypeView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val prefix: String = "houseConsignment.index.item.packageType"
+
+  def onPageLoad(arrivalId: ArrivalId, houseConsignmentIndex: Index, itemIndex: Index, packageIndex: Index, mode: Mode): Action[AnyContent] =
+    actions.requireData(arrivalId).async {
+      implicit request =>
+        service.getPackageTypes().map {
+          packageTypeList =>
+            val form = formProvider(prefix, packageTypeList)
+            val preparedForm = request.userAnswers.get(PackageTypePage(houseConsignmentIndex, itemIndex, packageIndex)) match {
+              case None        => form
+              case Some(value) => form.fill(value)
+            }
+            Ok(view(preparedForm, request.userAnswers.id, packageTypeList.values, mode, houseConsignmentIndex, itemIndex, packageIndex))
+        }
+    }
+
+  def onSubmit(arrivalId: ArrivalId, houseConsignmentIndex: Index, itemIndex: Index, packageIndex: Index, mode: Mode): Action[AnyContent] =
+    actions.requireData(arrivalId).async {
+      implicit request =>
+        service.getPackageTypes().flatMap {
+          packagesTypeList =>
+            val form: Form[PackageType] = formProvider(prefix, packagesTypeList)
+
+            form
+              .bindFromRequest()
+              .fold(
+                formWithErrors =>
+                  Future.successful(
+                    BadRequest(view(formWithErrors, request.userAnswers.id, packagesTypeList.values, mode, houseConsignmentIndex, itemIndex, packageIndex))
+                  ),
+                value => redirect(value, houseConsignmentIndex, itemIndex, packageIndex, mode)
+              )
+
+        }
+    }
+
+  private def redirect(
+    value: PackageType,
+    houseConsignmentIndex: Index,
+    itemIndex: Index,
+    packageIndex: Index,
+    mode: Mode
+  )(implicit request: MandatoryDataRequest[_]): Future[Result] =
+    for {
+      updatedAnswers <- Future.fromTry(request.userAnswers.set(PackageTypePage(houseConsignmentIndex, itemIndex, packageIndex), value))
+      _              <- sessionRepository.set(updatedAnswers)
+    } yield Redirect(navigator.nextPage(PackageTypePage(houseConsignmentIndex, itemIndex, packageIndex), mode, request.userAnswers))
+}
---
+++ test/views/houseConsignment/index/items/PackageTypeViewSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.houseConsignment.index.items
+
+import forms.SelectableFormProvider
+import generators.Generators
+import models.reference.PackageType
+import models.{NormalMode, SelectableList}
+import org.scalacheck.Arbitrary
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.InputSelectViewBehaviours
+import views.html.houseConsignment.index.items.PackageTypeView
+
+class PackageTypeViewSpec extends InputSelectViewBehaviours[PackageType] {
+
+  override def form: Form[PackageType] = new SelectableFormProvider()(prefix, SelectableList(values))
+
+  override def applyView(form: Form[PackageType]): HtmlFormat.Appendable =
+    injector.instanceOf[PackageTypeView].apply(form, arrivalId, values, NormalMode, houseConsignmentIndex, itemIndex, packageIndex)(fakeRequest, messages)
+
+  implicit override val arbitraryT: Arbitrary[PackageType] = arbitraryPackageType
+
+  override val prefix: String = "houseConsignment.index.item.packageType"
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithHeading()
+
+  behave like pageWithHint("Enter the package or code, like cylinder or CY.")
+
+  behave like pageWithSelect()
+
+  behave like pageWithSubmitButton("Continue")
+
+}
