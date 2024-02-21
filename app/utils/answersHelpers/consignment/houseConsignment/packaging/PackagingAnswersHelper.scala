c869fdd0aec41e7ca979ae395c96cb1c82b03637
joecefnhedog <joecefnhedog@gmail.com>
2024-02-21 11:20:26 +0000

    CTCP-4876: PR comments 1

---
+++ app/utils/answersHelpers/consignment/houseConsignment/packaging/PackagingAnswersHelper.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.answersHelpers.consignment.houseConsignment.packaging
+
+import models.reference.PackageType
+import models.{Index, UserAnswers}
+import pages.houseConsignment.index.items.packaging.{PackagingCountPage, PackagingMarksPage, PackagingTypePage}
+import pages.sections.PackagingSection
+import play.api.i18n.Messages
+import play.api.mvc.Call
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import uk.gov.hmrc.http.HttpVerbs.GET
+import utils.answersHelpers.AnswersHelper
+import viewModels.sections.Section
+import viewModels.sections.Section.AccordionSection
+
+class PackagingAnswersHelper(userAnswers: UserAnswers, houseConsignmentIndex: Index, itemIndex: Index)(implicit messages: Messages)
+    extends AnswersHelper(userAnswers) {
+
+  private[houseConsignment] def packageTypeRow(packageIndex: Index): Option[SummaryListRow] = getAnswerAndBuildRow[PackageType](
+    page = PackagingTypePage(houseConsignmentIndex, itemIndex, packageIndex),
+    formatAnswer = formatAsPackage,
+    prefix = "unloadingFindings.rowHeadings.item.packageType",
+    args = Seq(packageIndex.display, itemIndex.display): _*,
+    id = Some(s"change-package-type-${packageIndex.display}"),
+    call = Some(Call(GET, "#"))
+  )
+
+  private[houseConsignment] def packageMarksRow(packageIndex: Index): Option[SummaryListRow] = getAnswerAndBuildRow[String](
+    page = PackagingMarksPage(houseConsignmentIndex, itemIndex, packageIndex),
+    formatAnswer = formatAsText,
+    prefix = "unloadingFindings.rowHeadings.item.packageMarks",
+    args = Seq(packageIndex.display, itemIndex.display): _*,
+    id = Some(s"change-package-mark-${packageIndex.display}"),
+    call = Some(Call(GET, "#"))
+  )
+
+  private[houseConsignment] def packageCountRow(packageIndex: Index): Option[SummaryListRow] = getAnswerAndBuildRow[BigInt](
+    page = PackagingCountPage(houseConsignmentIndex, itemIndex, packageIndex),
+    formatAnswer = formatAsText,
+    prefix = "unloadingFindings.rowHeadings.item.packageCount",
+    args = Seq(packageIndex.display, itemIndex.display): _*,
+    id = Some(s"change-package-count-${packageIndex.display}"),
+    call = Some(Call(GET, "#"))
+  )
+
+  def packageSections: Seq[Section] =
+    userAnswers
+      .get(PackagingSection(houseConsignmentIndex, itemIndex))
+      .mapWithIndex {
+        case (_, packageIndex) =>
+          val rows = Seq(packageTypeRow(packageIndex), packageCountRow(packageIndex), packageMarksRow(packageIndex)).flatten
+
+          val section = AccordionSection(
+            sectionTitle = messages("unloadingFindings.subsections.packages", packageIndex.display),
+            rows = rows
+          )
+
+          section
+
+      }
+}
---
+++ test/utils/answersHelpers/consignment/houseConsignment/packaging/PackagingAnswersHelperSpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.answersHelpers.consignment.houseConsignment.packaging
+
+import models.reference.PackageType
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import pages.NetWeightPage
+import pages.houseConsignment.index.items.packaging.{PackagingCountPage, PackagingMarksPage, PackagingTypePage}
+import pages.houseConsignment.index.items.{GrossWeightPage, ItemDescriptionPage}
+import utils.answersHelpers.AnswersHelperSpecBase
+
+class PackagingAnswersHelperSpec extends AnswersHelperSpecBase {
+
+  "PackagingAnswersHelper" - {
+
+    "packageTypeRow" - {
+      val page = PackagingTypePage(hcIndex, itemIndex, packageIndex)
+      "must return None" - {
+        s"when $page undefined" in {
+          val helper = new PackagingAnswersHelper(emptyUserAnswers, hcIndex, itemIndex)
+          helper.packageTypeRow(packageIndex) mustBe None
+        }
+      }
+
+      "must return Some(Row)" - {
+        s"when $page defined" in {
+          forAll(arbitrary[PackageType]) {
+            value =>
+              val answers = emptyUserAnswers.setValue(page, value)
+
+              val helper = new PackagingAnswersHelper(answers, hcIndex, itemIndex)
+              val result = helper.packageTypeRow(packageIndex).value
+
+              result.key.value mustBe "Type"
+              result.value.value mustBe s"${value.asDescription}"
+              val action = result.actions.value.items.head
+              action.content.value mustBe "Change"
+              action.visuallyHiddenText.value mustBe "type of package 1 for item 1"
+              action.href mustBe "#"
+          }
+        }
+      }
+    }
+
+    "packageCountRow" - {
+      val page = PackagingCountPage(hcIndex, itemIndex, packageIndex)
+      "must return None" - {
+        s"when $page undefined" in {
+          val helper = new PackagingAnswersHelper(emptyUserAnswers, hcIndex, itemIndex)
+          helper.packageCountRow(packageIndex) mustBe None
+        }
+      }
+
+      "must return Some(Row)" - {
+        s"when $page defined" in {
+          forAll(arbitrary[BigInt]) {
+            value =>
+              val answers = emptyUserAnswers.setValue(page, value)
+
+              val helper = new PackagingAnswersHelper(answers, hcIndex, itemIndex)
+              val result = helper.packageCountRow(packageIndex).value
+
+              result.key.value mustBe "Quantity"
+              result.value.value mustBe s"$value"
+              val action = result.actions.value.items.head
+              action.content.value mustBe "Change"
+              action.visuallyHiddenText.value mustBe "quantity of package 1 for item 1"
+              action.href mustBe "#"
+          }
+        }
+      }
+    }
+
+    "packageMarksRow" - {
+      val page = PackagingMarksPage(hcIndex, itemIndex, packageIndex)
+      "must return None" - {
+        s"when $page undefined" in {
+          val helper = new PackagingAnswersHelper(emptyUserAnswers, hcIndex, itemIndex)
+          helper.packageMarksRow(packageIndex) mustBe None
+        }
+      }
+
+      "must return Some(Row)" - {
+        s"when $page defined" in {
+          forAll(arbitrary[String]) {
+            value =>
+              val answers = emptyUserAnswers.setValue(page, value)
+
+              val helper = new PackagingAnswersHelper(answers, hcIndex, itemIndex)
+              val result = helper.packageMarksRow(packageIndex).value
+
+              result.key.value mustBe "Shipping mark"
+              result.value.value mustBe s"$value"
+              val action = result.actions.value.items.head
+              action.content.value mustBe "Change"
+              action.visuallyHiddenText.value mustBe "shipping mark of package 1 for item 1"
+              action.href mustBe "#"
+          }
+        }
+      }
+    }
+
+  }
+}
