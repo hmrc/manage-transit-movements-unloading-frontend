88c939df984756a62b7487df4695566a21aaf673
malaw-hmrc <malawarayan.ganesan@digital.hmrc.gov.uk>
2024-02-22 16:48:25 +0000

    feat(ConsignmentTransformerSpec.scala): add tests

--- test/utils/transformers/ConsignmentTransformerSpec.scala
+++ test/utils/transformers/ConsignmentTransformerSpec.scala
@@ -24,6 +24,7 @@ import org.mockito.Mockito.when
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import pages.QuestionPage
+import pages.grossMass.GrossMassPage
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsObject, JsPath, Json}
@@ -105,6 +106,7 @@ class ConsignmentTransformerSpec extends SpecBase with AppWithDefaultMockFixture
           result.getValue(FakeDocumentsSection) mustBe Json.obj("foo" -> "bar")
           result.getValue(FakeHouseConsignmentSection) mustBe Json.obj("foo" -> "bar")
           result.getValue(FakeAdditionalReferenceSection) mustBe Json.obj("foo" -> "bar")
+          result.get(GrossMassPage) mustBe consignment.grossMass
       }
     }
 
@@ -116,6 +118,7 @@ class ConsignmentTransformerSpec extends SpecBase with AppWithDefaultMockFixture
       result.get(FakeDocumentsSection) must not be defined
       result.get(FakeHouseConsignmentSection) must not be defined
       result.get(FakeAdditionalReferenceSection) must not be defined
+      result.get(GrossMassPage) must not be defined
     }
   }
 }
