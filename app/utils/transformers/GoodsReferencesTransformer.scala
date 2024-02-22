f92eac8e05488906cc82f66a8631e821bce75e8d
joecefnhedog <joecefnhedog@gmail.com>
2024-02-22 16:34:33 +0000

    CTCP-4880: Unknown addition

--- app/utils/transformers/GoodsReferencesTransformer.scala
+++ app/utils/transformers/GoodsReferencesTransformer.scala
@@ -33,9 +33,7 @@ class GoodsReferencesTransformer @Inject() (implicit ec: ExecutionContext) exten
           userAnswers =>
             val itemIndex: Index = Index(i)
             val pipeline: UserAnswers => Future[UserAnswers] =
-              set(ItemPage(equipmentIndex, itemIndex), Item(declarationGoodsItemNumber.intValue, sequenceNumber)) andThen
-                set(ItemPage(equipmentIndex, itemIndex), Item(declarationGoodsItemNumber.intValue, sequenceNumber))
-
+              set(ItemPage(equipmentIndex, itemIndex), Item(declarationGoodsItemNumber.intValue, sequenceNumber))
             pipeline(userAnswers)
         }
     })
