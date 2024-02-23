3d87cd68f41baa2266059397a442af30baacac56
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2024-02-23 11:56:12 +0000

    CTCP-4885: Dangerous Goods

---
+++ app/utils/transformers/DangerousGoodsTransformer.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.transformers
+
+import generated.DangerousGoodsType01
+import models.{Index, UserAnswers}
+import pages.houseConsignment.index.items.DangerousGoodsPage
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DangerousGoodsTransformer @Inject() (implicit ec: ExecutionContext) extends PageTransformer {
+
+  def transform(dangerousGoods: Seq[DangerousGoodsType01], hcIndex: Index, itemIndex: Index): UserAnswers => Future[UserAnswers] = userAnswers =>
+    dangerousGoods.zipWithIndex.foldLeft(Future.successful(userAnswers))({
+      case (acc, (DangerousGoodsType01(_, unNumber), i)) =>
+        acc.flatMap {
+          userAnswers =>
+            val unNumberIndex = Index(i)
+            val pipeline: UserAnswers => Future[UserAnswers] =
+              set(DangerousGoodsPage(hcIndex, itemIndex, unNumberIndex), unNumber)
+
+            pipeline(userAnswers)
+        }
+    })
+}
