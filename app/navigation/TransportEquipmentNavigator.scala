8dd2a45421fa0900a0b5d472e0c83aa38659ae4b
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2024-03-07 13:40:33 +0000

    CTCP-4605: Created TransportEquipmentNavigator and implemented the checkRoutes

---
+++ app/navigation/TransportEquipmentNavigator.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import com.google.inject.Singleton
+import models.UserAnswers
+import pages._
+import pages.transportEquipment.index.seals.SealIdentificationNumberPage
+import play.api.mvc.Call
+
+@Singleton
+class TransportEquipmentNavigator extends Navigator {
+
+  override def normalRoutes: PartialFunction[Page, UserAnswers => Option[Call]] = ???
+
+  override def checkRoutes: PartialFunction[Page, UserAnswers => Option[Call]] = {
+    case ContainerIdentificationNumberPage(_) => ua => Some(controllers.routes.UnloadingFindingsController.onPageLoad(ua.id))
+    case SealIdentificationNumberPage(_, _)   => ua => Some(controllers.routes.UnloadingFindingsController.onPageLoad(ua.id))
+  }
+}
---
+++ test/navigation/TransportEquipmentNavigatorSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import base.SpecBase
+import generators.Generators
+import models._
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import pages.ContainerIdentificationNumberPage
+import pages.documents.{AdditionalInformationPage, DocumentReferenceNumberPage, TypePage}
+import pages.transportEquipment.index.seals.SealIdentificationNumberPage
+
+class TransportEquipmentNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with Generators {
+
+  val navigator = new TransportEquipmentNavigator
+
+  "TransportNavigator" - {
+
+    "in CheckMode" - {
+
+      val mode = CheckMode
+
+      "must go from ContainerIdentificationNumber page to UnloadingFindings page" in {
+
+        val userAnswers = emptyUserAnswers.setValue(ContainerIdentificationNumberPage(equipmentIndex), "container1")
+
+        navigator
+          .nextPage(ContainerIdentificationNumberPage(equipmentIndex), mode, userAnswers)
+          .mustBe(controllers.routes.UnloadingFindingsController.onPageLoad(arrivalId))
+      }
+
+      "must go from SealIdentificationNumber page to UnloadingFindings page" in {
+
+        val userAnswers = emptyUserAnswers.setValue(SealIdentificationNumberPage(equipmentIndex, sealIndex), "seal1")
+
+        navigator
+          .nextPage(SealIdentificationNumberPage(equipmentIndex, sealIndex), mode, userAnswers)
+          .mustBe(controllers.routes.UnloadingFindingsController.onPageLoad(arrivalId))
+      }
+    }
+  }
+}
