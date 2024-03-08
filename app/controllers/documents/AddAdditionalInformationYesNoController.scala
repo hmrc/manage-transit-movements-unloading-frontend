e8ee1044a8b4ad62ef20020dd03657d04a35f063
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-03-08 15:20:51 +0000

    CTCP-4652: update view spec

---
+++ app/controllers/documents/AddAdditionalInformationYesNoController.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.documents
+
+import controllers.actions._
+import forms.YesNoFormProvider
+import models.{ArrivalId, Index, Mode}
+import navigation.Navigator
+import pages.documents.AddAdditionalInformationYesNoPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.documents.AddAdditionalInformationYesNoView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class AddAdditionalInformationYesNoController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  actions: Actions,
+  formProvider: YesNoFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: AddAdditionalInformationYesNoView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider("document.addAdditionalInformationYesNo")
+
+  def onPageLoad(arrivalId: ArrivalId, mode: Mode, documentIndex: Index): Action[AnyContent] = actions.getStatus(arrivalId) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(AddAdditionalInformationYesNoPage(documentIndex)) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, request.userAnswers.mrn, arrivalId, documentIndex, mode))
+  }
+
+  def onSubmit(arrivalId: ArrivalId, mode: Mode, documentIndex: Index): Action[AnyContent] = actions.getStatus(arrivalId).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, request.userAnswers.mrn, arrivalId, documentIndex, mode))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(AddAdditionalInformationYesNoPage(documentIndex), value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(AddAdditionalInformationYesNoPage(documentIndex), mode, updatedAnswers))
+        )
+  }
+}
