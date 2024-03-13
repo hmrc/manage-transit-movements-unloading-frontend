604d014bb88e712e9a939526b88f4944738f28db
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2024-03-13 16:03:05 +0000

    CTCP-4651: Implemented dynamic content for consignment level document TypePage in NormalMode

---
+++ test/viewModels/documents/TypeViewModelSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels.documents
+
+import base.SpecBase
+import generators.Generators
+import models.{CheckMode, NormalMode}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import viewModels.documents.TypeViewModel.TypeViewModelProvider
+
+class TypeViewModelSpec extends SpecBase with ScalaCheckPropertyChecks with Generators {
+
+  "must create view model" - {
+    "when Normal mode" in {
+      val viewModelProvider = new TypeViewModelProvider()
+      val result            = viewModelProvider.apply(NormalMode)
+
+      result.title mustBe "What type of document do you want to add?"
+      result.heading mustBe "What type of document do you want to add?"
+    }
+
+    "when Check mode" in {
+      val viewModelProvider = new TypeViewModelProvider()
+      val result            = viewModelProvider.apply(CheckMode)
+
+      result.title mustBe "What is the new document type?"
+      result.heading mustBe "What is the new document type?"
+    }
+  }
+}
