f78845339a4376dc1e16d4652b91aee5a88055d9
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2024-03-27 15:07:50 +0000

    CTCP-4566: Implemented AddCommodityCodeYesNo page

---
+++ test/views/houseConsignment/index/items/AddCommodityCodeYesNoViewSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.houseConsignment.index.items
+
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.YesNoViewBehaviours
+import views.html.houseConsignment.index.items.AddCommodityCodeYesNoView
+
+class AddCommodityCodeYesNoViewSpec extends YesNoViewBehaviours {
+
+  override def applyView(form: Form[Boolean]): HtmlFormat.Appendable =
+    injector
+      .instanceOf[AddCommodityCodeYesNoView]
+      .apply(form, mrn, arrivalId, houseConsignmentIndex, itemIndex, NormalMode)(fakeRequest, messages)
+
+  override val prefix: String = "houseConsignment.item.addCommodityCodeYesNo"
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithCaption(s"This notification is MRN: ${mrn.toString}")
+
+  behave like pageWithHeading()
+
+  behave like pageWithContent("p", "This is an internationally recognised reference number used to identify the specific product in transit.")
+
+  behave like pageWithRadioItems()
+
+  behave like pageWithSubmitButton("Continue")
+}
