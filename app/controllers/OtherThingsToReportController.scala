81930eb267923a410e38128e23036d19b556de3c
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2024-04-04 10:02:40 +0100

    CTCP-4531: Other Things To Report Page

---
+++ app/controllers/OtherThingsToReportController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import forms.OtherThingsToReportFormProvider
+import forms.Constants.otherThingsToReportLength
+import models.{ArrivalId, Mode}
+import navigation.Navigation
+import pages.OtherThingsToReportPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.OtherThingsToReportView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class OtherThingsToReportController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigation,
+  actions: Actions,
+  formProvider: OtherThingsToReportFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: OtherThingsToReportView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider()
+
+  def onPageLoad(arrivalId: ArrivalId, mode: Mode): Action[AnyContent] =
+    actions.getStatus(arrivalId) {
+      implicit request =>
+        val preparedForm = request.userAnswers.get(OtherThingsToReportPage) match {
+          case None        => form
+          case Some(value) => form.fill(value)
+        }
+
+        Ok(view(preparedForm, request.userAnswers.mrn, arrivalId, otherThingsToReportLength, mode))
+    }
+
+  def onSubmit(arrivalId: ArrivalId, mode: Mode): Action[AnyContent] =
+    actions.getStatus(arrivalId).async {
+      implicit request =>
+        form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => Future.successful(BadRequest(view(formWithErrors, request.userAnswers.mrn, arrivalId, otherThingsToReportLength, mode))),
+            value =>
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.set(OtherThingsToReportPage, value))
+                _              <- sessionRepository.set(updatedAnswers)
+              } yield Redirect(navigator.nextPage(OtherThingsToReportPage, mode, updatedAnswers))
+          )
+    }
+}
---
+++ test/forms/OtherThingsToReportFormProviderSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import models.messages.UnloadingRemarksRequest.stringFieldRegexComma
+import org.scalacheck.Gen
+import play.api.data.{Field, FormError}
+import wolfendale.scalacheck.regexp.RegexpGen
+
+import scala.util.Random
+
+class OtherThingsToReportFormProviderSpec extends StringFieldBehaviours {
+
+  val requiredKey = "otherThingsToReport.error.required"
+  val lengthKey   = "otherThingsToReport.error.length"
+  val invalidKey  = "otherThingsToReport.error.invalid"
+  val maxLength   = 512
+
+  val form = new OtherThingsToReportFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+    "must not bind strings with invalid characters" in {
+
+      val generator: Gen[String] = RegexpGen.from(s"[!£^*(){}_+=:;|`~<>±üçñèé]{$length}")
+
+      forAll(generator) {
+        invalidString =>
+          val result: Field = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
+          result.errors must contain(FormError(fieldName, invalidKey, Seq(stringFieldRegexComma.toString())))
+      }
+    }
+
+    s"must not bind strings longer than $maxLength characters" in {
+      val invalidString = Random.alphanumeric.take(maxLength + 1).mkString
+
+      val result: Field = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
+
+      result.errors must contain(FormError(fieldName, lengthKey, List(maxLength)))
+    }
+
+  }
+}
