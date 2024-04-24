8c7d21b1362eb3a98d58f275d89231eff9ceb5f6
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-04-24 09:25:01 +0100

    CTCP-4734: create AddAdditionalInformationYesNoPage at houseConsignmnet.index.document level

---
+++ test/controllers/houseConsignment/index/documents/AddAdditionalInformationYesNoControllerSpec.scala
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.houseConsignment.index.documents
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.YesNoFormProvider
+import models.NormalMode
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import pages.houseConsignment.index.documents.AddAdditionalInformationYesNoPage
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.houseConsignment.index.documents.AddAdditionalInformationYesNoView
+
+import scala.concurrent.Future
+
+class AddAdditionalInformationYesNoControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
+
+  private val formProvider = new YesNoFormProvider()
+  private val form         = formProvider("document.addAdditionalInformationYesNo")
+  private val mode         = NormalMode
+
+  private lazy val addAdditionalInformationYesNoRoute =
+    controllers.houseConsignment.index.documents.routes.AddAdditionalInformationYesNoController
+      .onPageLoad(arrivalId, mode, houseConsignmentIndex, documentIndex)
+      .url
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+
+  "AddAdditionalInformationYesNoController" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, addAdditionalInformationYesNoRoute)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[AddAdditionalInformationYesNoView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, mrn, arrivalId, houseConsignmentIndex, documentIndex, mode)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(AddAdditionalInformationYesNoPage(houseConsignmentIndex, documentIndex), true)
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, addAdditionalInformationYesNoRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "true"))
+
+      val view = injector.instanceOf[AddAdditionalInformationYesNoView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, mrn, arrivalId, houseConsignmentIndex, documentIndex, mode)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val request = FakeRequest(POST, addAdditionalInformationYesNoRoute)
+        .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val invalidAnswer = ""
+
+      val request    = FakeRequest(POST, addAdditionalInformationYesNoRoute).withFormUrlEncodedBody(("value", ""))
+      val filledForm = form.bind(Map("value" -> invalidAnswer))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[AddAdditionalInformationYesNoView]
+
+      contentAsString(result) mustEqual
+        view(filledForm, mrn, arrivalId, houseConsignmentIndex, documentIndex, mode)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, addAdditionalInformationYesNoRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, addAdditionalInformationYesNoRoute)
+        .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    }
+  }
+}
