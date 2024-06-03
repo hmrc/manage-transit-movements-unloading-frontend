8814d6ad8ee68aaedb158b4a88e918297a106f05
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-06-03 09:32:09 +0100

    CTCP-5376: Added metrics for submissions.

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -23,7 +23,7 @@ import models.ArrivalId
 import models.AuditType.UnloadingRemarks
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.submission.{AuditService, SubmissionService}
+import services.submission.{AuditService, MetricsService, SubmissionService}
 import uk.gov.hmrc.http.HttpReads.is2xx
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import viewModels.CheckYourAnswersViewModel
@@ -33,15 +33,16 @@ import views.html.CheckYourAnswersView
 import scala.concurrent.ExecutionContext
 
 class CheckYourAnswersController @Inject() (
-  override val messagesApi: MessagesApi,
-  actions: Actions,
-  val controllerComponents: MessagesControllerComponents,
-  view: CheckYourAnswersView,
-  viewModelProvider: CheckYourAnswersViewModelProvider,
-  submissionService: SubmissionService,
-  auditService: AuditService
-)(implicit val executionContext: ExecutionContext)
-    extends FrontendBaseController
+                                             override val messagesApi: MessagesApi,
+                                             actions: Actions,
+                                             val controllerComponents: MessagesControllerComponents,
+                                             view: CheckYourAnswersView,
+                                             viewModelProvider: CheckYourAnswersViewModelProvider,
+                                             submissionService: SubmissionService,
+                                             auditService: AuditService,
+                                             metricsService: MetricsService
+                                           )(implicit val executionContext: ExecutionContext)
+  extends FrontendBaseController
     with I18nSupport
     with Logging {
 
@@ -58,9 +59,11 @@ class CheckYourAnswersController @Inject() (
       implicit request =>
         submissionService.submit(request.userAnswers, arrivalId).map {
           response =>
+            val auditType = UnloadingRemarks
+            metricsService.increment(auditType.name, response)
             response.status match {
               case x if is2xx(x) =>
-                auditService.audit(UnloadingRemarks, request.userAnswers)
+                auditService.audit(auditType, request.userAnswers)
                 Redirect(controllers.routes.UnloadingRemarksSentController.onPageLoad(arrivalId))
               case x =>
                 logger.error(s"Error submitting IE044: $x")
