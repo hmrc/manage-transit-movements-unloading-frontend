db9211775ffe46ebba96bcc437abb89d4151332c
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-06-20 07:35:47 +0100

    updateDep: Update AppDependencies and plugins to latest versions

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -24,7 +24,7 @@ import play.api.Logging
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.submission.{AuditService, MetricsService, SubmissionService}
-import uk.gov.hmrc.http.HttpReads.is2xx
+import uk.gov.hmrc.http.HttpErrorFunctions.is2xx
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import viewModels.CheckYourAnswersViewModel
 import viewModels.CheckYourAnswersViewModel.CheckYourAnswersViewModelProvider
