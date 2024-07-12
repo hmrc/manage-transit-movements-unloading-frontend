b2a09b03e10ccb8007a5f5e06028bb785838e269
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-12 10:10:08 +0100

    CTCP-5528: write new spec for UnloadingFindingsViewModel

--- test/viewModels/UnloadingFindingsViewModelSpec.scala
+++ test/viewModels/UnloadingFindingsViewModelSpec.scala
@@ -29,76 +29,7 @@ class UnloadingFindingsViewModelSpec extends SpecBase {
     }
 
     "return correct title for newAuth and goodsTooLarge = false" in {
-      viewModel.title(newAuth = true, goodsTooLarge = false) mustBe "unloadingGuidance.newAuth.goodsTooLargeNo.title"
-    }
-
-    "return correct title for newAuth and goodsTooLarge = true" in {
-      viewModel.title(newAuth = true, goodsTooLarge = true) mustBe "unloadingGuidance.newAuth.goodsTooLargeYes.title"
-    }
-
-    "return correct heading for not newAuth" in {
-      viewModel.heading(newAuth = false, goodsTooLarge = false) mustBe "unloadingGuidance.notNewAuth.heading"
-      viewModel.heading(newAuth = false, goodsTooLarge = true) mustBe "unloadingGuidance.notNewAuth.heading"
-    }
-
-    "return correct heading for newAuth and goodsTooLarge = false" in {
-      viewModel.heading(newAuth = true, goodsTooLarge = false) mustBe "unloadingGuidance.newAuth.goodsTooLargeNo.heading"
-    }
-
-    "return correct heading for newAuth and goodsTooLarge = true" in {
-      viewModel.heading(newAuth = true, goodsTooLarge = true) mustBe "unloadingGuidance.newAuth.goodsTooLargeYes.heading"
-    }
-
-    "return correct preLinkText for newAuth and goodsTooLarge = false" in {
-      viewModel.preLinkText(newAuth = true, goodsTooLarge = false) mustBe "unloadingGuidance.preLinkText"
-    }
-
-    "return empty preLinkText for not newAuth" in {
-      viewModel.preLinkText(newAuth = false, goodsTooLarge = false) mustBe ""
-      viewModel.preLinkText(newAuth = false, goodsTooLarge = true) mustBe ""
-    }
-
-    "return empty preLinkText for newAuth and goodsTooLarge = true" in {
-      viewModel.preLinkText(newAuth = true, goodsTooLarge = true) mustBe ""
-    }
-
-    "return correct postLinkText for newAuth and goodsTooLarge = false" in {
-      viewModel.postLinkText(newAuth = true, goodsTooLarge = false) mustBe "unloadingGuidance.postLinkText"
-    }
-
-    "return empty postLinkText for not newAuth" in {
-      viewModel.postLinkText(newAuth = false, goodsTooLarge = false) mustBe ""
-      viewModel.postLinkText(newAuth = false, goodsTooLarge = true) mustBe ""
-    }
-
-    "return empty postLinkText for newAuth and goodsTooLarge = true" in {
-      viewModel.postLinkText(newAuth = true, goodsTooLarge = true) mustBe ""
-    }
-
-    "return correct para1 for newAuth and goodsTooLarge = true" in {
-      viewModel.para1(newAuth = true, goodsTooLarge = true) mustBe "unloadingGuidance.para1"
-    }
-
-    "return empty para1 for not newAuth" in {
-      viewModel.para1(newAuth = false, goodsTooLarge = false) mustBe ""
-      viewModel.para1(newAuth = false, goodsTooLarge = true) mustBe ""
-    }
-
-    "return empty para1 for newAuth and goodsTooLarge = false" in {
-      viewModel.para1(newAuth = true, goodsTooLarge = false) mustBe ""
-    }
-
-    "return correct para2 for not newAuth" in {
-      viewModel.para2(newAuth = false, goodsTooLarge = false) mustBe "unloadingGuidance.para2.notNewAuth"
-      viewModel.para2(newAuth = false, goodsTooLarge = true) mustBe "unloadingGuidance.para2.notNewAuth"
-    }
-
-    "return empty para2 for newAuth and goodsTooLarge = false" in {
-      viewModel.para2(newAuth = true, goodsTooLarge = false) mustBe ""
-    }
-
-    "return correct para2 for newAuth and goodsTooLarge = true" in {
-      viewModel.para2(newAuth = true, goodsTooLarge = true) mustBe "unloadingGuidance.para2.newAuth.goodsTooLargeYes"
+      viewModel.title(newAuth = true, goodsTooLarge = false) mustBe ("unloadingGuidance.newAuth.goodsTooLargeNo.title")
     }
 
   }
