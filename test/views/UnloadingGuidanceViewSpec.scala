c166cf26bc20e702286f8f429d5becc7092f837f
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-12 12:10:28 +0100

    CTCP-5528: update view spec

--- test/views/UnloadingGuidanceViewSpec.scala
+++ test/views/UnloadingGuidanceViewSpec.scala
@@ -52,3 +52,70 @@ class UnloadingGuidanceViewSpec extends ViewBehaviours {
 
   behave like pageWithSubmitButton("Continue")
 }
+
+class UnloadingGuidanceViewSpecNewAuthGoodsTooLargeSpec extends ViewBehaviours {
+
+  private val viewModel = new UnloadingGuidanceViewModel(true, true)
+
+  override def view: HtmlFormat.Appendable =
+    injector.instanceOf[UnloadingGuidanceView].apply(mrn, arrivalId, messageId, NormalMode, viewModel)(fakeRequest, messages)
+
+  override val prefix: String = "unloadingGuidance.newAuth.goodsTooLargeYes"
+
+  behave like pageWithTitle(true, true)
+
+  behave like pageWithBackLink()
+
+  behave like pageWithCaption(s"This notification is MRN: ${mrn.toString}")
+
+  behave like pageWithHeading(true, true)
+
+  behave like pageWithContent(
+    "p",
+    s"When checking the goods, make sure that the goods match the unloading permission for Movement Reference Number (MRN) $mrn. Take note of any discrepancies as you will need to include them in your unloading remarks."
+  )
+
+  behave like pageWithLink(
+    id = "download",
+    expectedText = "Download the Unloading Permission PDF",
+    expectedHref = s"http://localhost:9485/manage-transit-movements/${arrivalId.value}/unloading-permission-document/$messageId"
+  )
+
+  behave like pageWithSubmitButton("Continue")
+}
+
+class UnloadingGuidanceViewSpecNewAuthGoodsNotTooLargeSpec extends ViewBehaviours {
+
+  private val viewModel = new UnloadingGuidanceViewModel(true, false)
+
+  override def view: HtmlFormat.Appendable =
+    injector.instanceOf[UnloadingGuidanceView].apply(mrn, arrivalId, messageId, NormalMode, viewModel)(fakeRequest, messages)
+
+  override val prefix: String = "unloadingGuidance.newAuth.goodsTooLargeNo"
+
+  behave like pageWithTitle(true, false)
+
+  behave like pageWithBackLink()
+
+  behave like pageWithCaption(s"This notification is MRN: ${mrn.toString}")
+
+  behave like pageWithHeading(true, true)
+
+  behave like pageWithContent(
+    "p",
+    s"If you suspect any discrepancies, you must select no to using the revised unloading procedure. You will then need to unload the goods and report any discrepancies."
+  )
+
+  behave like pageWithLink(
+    id = "download",
+    expectedText = "Download the Unloading Permission PDF",
+    expectedHref = s"http://localhost:9485/manage-transit-movements/${arrivalId.value}/unloading-permission-document/$messageId"
+  )
+
+  behave like pageWithLink(
+    id = "download1",
+    expectedText = "select no to using the revised unloading procedure.",
+    expectedHref = s"/manage-transit-movements/unloading/${arrivalId.value}/revised-unloading-procedure"
+  )
+  behave like pageWithSubmitButton("Continue")
+}
