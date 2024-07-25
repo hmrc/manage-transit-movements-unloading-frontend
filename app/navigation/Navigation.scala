fa68bb7611ce8ce14e3775e9aff87aaa5ba593d2
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-07-25 11:28:06 +0100

    CTCP-5518: Add the rest of the navigation for LargeUnsealedGoodsRecordDiscrepanciesYesNoPage and SealsReplacedByCustomsAuthorityYesNoPage, as well as, checking to ensure navigation from other things to report page and cya is correct

--- app/navigation/Navigation.scala
+++ app/navigation/Navigation.scala
@@ -37,6 +37,8 @@ class Navigation extends Navigator {
     case OtherThingsToReportPage                             => ua => Some(routes.CheckYourAnswersController.onPageLoad(ua.id))
     case NewAuthYesNoPage                                    => ua => newAuthNavigation(ua)
     case GoodsTooLargeForContainerYesNoPage                  => ua => Some(routes.UnloadingGuidanceController.onPageLoad(ua.id))
+    case LargeUnsealedGoodsRecordDiscrepanciesYesNoPage      => ua => largeUnsealedGoodsDiscrepanciesYesNoNavigation(ua)
+    case SealsReplacedByCustomsAuthorityYesNoPage            => ua => Some(routes.OtherThingsToReportController.onPageLoad(ua.id, NormalMode))
   }
 
   override def checkRoutes: PartialFunction[Page, UserAnswers => Option[Call]] = {
@@ -54,6 +56,14 @@ class Navigation extends Navigator {
         Some(routes.GoodsTooLargeForContainerYesNoController.onPageLoad(ua.id, NormalMode))
       case _ =>
         Some(routes.UnloadingGuidanceController.onPageLoad(ua.id))
+    }
+
+  private def largeUnsealedGoodsDiscrepanciesYesNoNavigation(ua: UserAnswers): Option[Call] =
+    ua.get(LargeUnsealedGoodsRecordDiscrepanciesYesNoPage) match {
+      case Some(true) =>
+        Some(routes.NewAuthYesNoController.onPageLoad(ua.id, NormalMode))
+      case _ =>
+        Some(routes.CheckYourAnswersController.onPageLoad(ua.id))
     }
 
   private def stateOfSealsNormalNavigation(ua: UserAnswers): Option[Call] =
