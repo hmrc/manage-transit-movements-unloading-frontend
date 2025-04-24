9ec076ffa0965df5897f2a8a9fc792e0cfe35445
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-04-24 16:20:00 +0100

    CTCP-6421 Create pages and controllers for UCR
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

---
+++ test/controllers/UniqueConsignmentReferenceControllerSpec.scala
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.UniqueConsignmentReferenceFormProvider
+import generators.Generators
+import models.NormalMode
+import navigation.Navigation
+import org.apache.pekko.http.scaladsl.model.HttpHeader.ParsingResult.Ok
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import org.scalacheck.Arbitrary.arbitrary
+import pages.UniqueConsignmentReferencePage
+import play.api.http.Status.OK
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, route, status, writeableOf_AnyContentAsEmpty}
+import uk.gov.hmrc.http.HttpVerbs.GET
+import viewModels.UniqueConsignmentReferenceViewModel
+import viewModels.UniqueConsignmentReferenceViewModel.UniqueConsignmentReferenceViewModelProvider
+import views.html.UniqueConsignmentReferenceView
+
+class UniqueConsignmentReferenceControllerSpec extends SpecBase with AppWithDefaultMockFixtures with Generators {
+
+  private val formProvider                                   = new UniqueConsignmentReferenceFormProvider()
+  private val prefix                                         = "uniqueConsignmentReference"
+  private val form                                           = formProvider("uniqueConsignmentReference")
+  private val viewModel: UniqueConsignmentReferenceViewModel = arbitrary[UniqueConsignmentReferenceViewModel].sample.value
+  private val mockViewModelProvider                          = mock[UniqueConsignmentReferenceViewModelProvider]
+  private val mode                                           = NormalMode
+
+  lazy val ucrRoute: String = routes.UniqueConsignmentReferenceController.onPageLoad(arrivalId, mode).url
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockViewModelProvider)
+    when(mockViewModelProvider.apply(any())(any())).thenReturn(viewModel)
+  }
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .configure("feature-flags.phase-6-enabled" -> true)
+      .overrides(
+        bind[Navigation].toInstance(fakeNavigation),
+        bind[UniqueConsignmentReferenceViewModelProvider].toInstance(mockViewModelProvider)
+      )
+
+  "uniqueConsignmentReferenceController" - {
+    "must return OK and the correct view for a GET" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, ucrRoute)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[UniqueConsignmentReferenceView]
+
+      status(result) mustEqual Ok
+
+      contentAsString(result) mustEqual
+        view(form, mrn, arrivalId, mode, viewModel)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(UniqueConsignmentReferencePage, "test string")
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, ucrRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "GB"))
+
+      val view = injector.instanceOf[UniqueConsignmentReferenceView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, mrn, arrivalId, mode, viewModel)(request, messages).toString
+    }
+    //
+    //    "must redirect to the next page when valid data is submitted" in {
+    //
+    //      when(mockSessionRepository.set(any())) `thenReturn` Future.successful(true)
+    //      when(mockReferenceDataService.getCountries()(any())).thenReturn(Future.successful(countries))
+    //
+    //      setExistingUserAnswers(emptyUserAnswers)
+    //
+    //      val request =
+    //        FakeRequest(POST, countryRoute)
+    //          .withFormUrlEncodedBody((field, "GB"))
+    //
+    //      val result = route(app, request).value
+    //
+    //      status(result) mustEqual SEE_OTHER
+    //      redirectLocation(result).value mustEqual
+    //        routes.CountryController.onPageLoad(arrivalId, index, mode).url // TODO - Update redirect logic when other pages built CTCP-6428
+    //    }
+    //
+    //    "must return a Bad Request and errors when invalid data is submitted" in {
+    //      when(mockReferenceDataService.getCountries()(any())).thenReturn(Future.successful(countries))
+    //
+    //      setExistingUserAnswers(emptyUserAnswers)
+    //
+    //      val request = FakeRequest(POST, countryRoute).withFormUrlEncodedBody((field, ""))
+    //      val boundForm = form.bind(Map(field -> ""))
+    //
+    //      val result = route(app, request).value
+    //
+    //      status(result) mustEqual BAD_REQUEST
+    //
+    //      val view = injector.instanceOf[CountryView]
+    //
+    //      contentAsString(result) mustEqual
+    //        view(boundForm, countries, mrn, arrivalId, index, mode, viewModel)(request, messages).toString
+    //    }
+    //
+    //    "must redirect to Session Expired for a GET if no existing data is found" in {
+    //      setNoExistingUserAnswers()
+    //
+    //      val request = FakeRequest(GET, countryRoute)
+    //
+    //      val result = route(app, request).value
+    //
+    //      status(result) mustEqual SEE_OTHER
+    //
+    //      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    //    }
+    //
+    //    "must redirect to page not found for a GET if phase 6 is disabled" in {
+    //      val app = super
+    //        .guiceApplicationBuilder()
+    //        .configure("feature-flags.phase-6-enabled" -> false)
+    //        .build()
+    //
+    //      running(app) {
+    //
+    //        setExistingUserAnswers(emptyUserAnswers)
+    //
+    //        val request = FakeRequest(GET, countryRoute)
+    //
+    //        val result = route(app, request).value
+    //
+    //        status(result) mustEqual SEE_OTHER
+    //
+    //        redirectLocation(result).value mustEqual controllers.routes.ErrorController.notFound().url
+    //      }
+    //    }
+    //
+    //    "must redirect to Session Expired for a POST if no existing data is found" in {
+    //      setNoExistingUserAnswers()
+    //
+    //      val request =
+    //        FakeRequest(POST, countryRoute)
+    //          .withFormUrlEncodedBody((field, "answer"))
+    //
+    //      val result = route(app, request).value
+    //
+    //      status(result) mustEqual SEE_OTHER
+    //
+    //      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+    //    }
+    //
+    //    "must redirect to page not found for a POST if phase 6 is disabled" in {
+    //      val app = super
+    //        .guiceApplicationBuilder()
+    //        .configure("feature-flags.phase-6-enabled" -> false)
+    //        .build()
+    //
+    //      running(app) {
+    //
+    //        setExistingUserAnswers(emptyUserAnswers)
+    //
+    //        val request = FakeRequest(POST, countryRoute)
+    //          .withFormUrlEncodedBody((field, "answer"))
+    //
+    //        val result = route(app, request).value
+    //
+    //        status(result) mustEqual SEE_OTHER
+    //
+    //        redirectLocation(result).value mustEqual controllers.routes.ErrorController.notFound().url
+    //      }
+    //    }
+    //  }
+
+  }
+}
---
+++ test/views/UniqueConsignmentReferenceViewSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import forms.UniqueConsignmentReferenceFormProvider
+import generators.Generators
+import models.NormalMode
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Arbitrary.arbitrary
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import viewModels.{InputSize, UniqueConsignmentReferenceViewModel}
+import views.behaviours.InputTextViewBehaviours
+import views.html.UniqueConsignmentReferenceView
+
+class UniqueConsignmentReferenceViewSpec extends InputTextViewBehaviours[String] with Generators {
+
+  override val prefix: String = "uniqueConsignmentReference"
+
+  override def form: Form[String] = new UniqueConsignmentReferenceFormProvider()(prefix)
+
+  private val viewModel: UniqueConsignmentReferenceViewModel = arbitrary[UniqueConsignmentReferenceViewModel].sample.value
+
+  override def applyView(form: Form[String]): HtmlFormat.Appendable =
+    injector.instanceOf[UniqueConsignmentReferenceView].apply(form, mrn, arrivalId, NormalMode, viewModel)(fakeRequest, messages)
+
+  implicit override val arbitraryT: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+
+  behave like pageWithTitle(viewModel.title)
+
+  behave like pageWithBackLink()
+
+  behave like pageWithHeading(viewModel.heading)
+
+  behave like pageWithHint("This can be up to 70 characters long and include both letters and numbers.")
+
+  behave like pageWithInputText(Some(InputSize.Width20))
+
+  behave like pageWithSubmitButton("Continue")
+}
