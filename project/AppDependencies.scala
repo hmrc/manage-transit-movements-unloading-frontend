b1309d531fbd1a10e84035462fa9612a285b4f45
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-16 11:29:02 +0000

    Upgrade bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"              % bootstrapVersion,
     "org.apache.commons"    % "commons-text"                            % "1.14.0",
     "org.typelevel"        %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.22.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.31.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                     % "8.4.0",
     "javax.xml.bind"        % "jaxb-api"                                % "2.3.1",
     caffeine
