a8715eb5bc5f6367762997e4a626b23233852a70
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-16 12:02:42 +0000

    Upgrade libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.11.0"
-  private val bootstrapVersion = "10.4.0"
+  private val mongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"              % bootstrapVersion,
     "org.apache.commons"    % "commons-text"                            % "1.14.0",
     "org.typelevel"        %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.22.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.31.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                     % "8.4.0",
     "javax.xml.bind"        % "jaxb-api"                                % "2.3.1",
     caffeine
